fb44c7681efdbd5ba5693e823ee271d08c5ce7e8
imtiaz <programmer.imtiaz@gmail.com>
2020-04-10 16:26:15 +0600

    Video and Audio playing done

---
+++ app/src/main/java/com/imtiaz/playtime/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.imtiaz.playtime
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var player : SimpleExoPlayer? = null
+    private var mPlayWhenReady = true
+    private var mPlayerCurrentWindow = 0
+    private var mPlaybackPosition : Long =  0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun initializePlayer(){
+        player = ExoPlayerFactory.newSimpleInstance(this)
+        playerView.player = player
+
+        val uri = Uri.parse(getString(R.string.media_url_mp4))
+        val mediaSource = buildMediaSource(uri)
+
+        player?.apply {
+            playWhenReady = mPlayWhenReady
+            seekTo(mPlayerCurrentWindow, mPlaybackPosition)
+            prepare(mediaSource, false, false)
+        }
+    }
+
+    private fun buildMediaSource(uri : Uri) : MediaSource{
+        val dataSourceFactory = DefaultDataSourceFactory(this, resources.getString(R.string.app_name))
+        return ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
+    }
+
+
+    private fun hideSystemUi(){
+        playerView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+    }
+
+    private fun releasePlayer(){
+        player?.apply {
+            mPlayWhenReady = playWhenReady
+            mPlaybackPosition = currentPosition
+            mPlayerCurrentWindow = currentWindowIndex
+            release()
+            player = null
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if(Util.SDK_INT >= 24) initializePlayer()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if((Util.SDK_INT < 24 || player == null)){
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+
+}
